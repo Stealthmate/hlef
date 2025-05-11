949f4b192d6270a63f0d62ce5d21da948dc35ed8
stealthmate <stealthmate1233@gmail.com>
2025-05-11 10:51:54 +0900

    fix: incorrect parsing of posting comments
    I found an edge case where comment parsing would not work as expected
    (the new test case) and so I rewrote a bit of the parsing logic in order
    to be able to better handle that.

--- src/parse.rs
+++ src/parse.rs
@@ -4,7 +4,9 @@ const COMMENT_REGEX: &str = r"^;(.*)$";
 const TRANSACTION_HEAD_REGEX: &str = r"^[0-9]{4}-[0-9]{2}-[0-9]{2}.*$";
 const POSTING_ACCOUNT_REGEX: &str = r"^   *([^ ]+) *";
 const POSTING_ASSERTION_REGEX: &str = r"^([=*]+) *";
-const POSTING_AMOUNT_REGEX: &str = r"^([^0-9 .,]+)? *([\-0-9,.]+) *";
+const WHITESPACE_REGEX: &str = r"^ *";
+const COMMODITY_REGEX: &str = r"^[^0-9 .,;]+";
+const AMOUNT_REGEX: &str = r"^[\-0-9,.]+";
 const POSTING_COMMENT_REGEX: &str = r"^   *;(.*)$";
 
 #[derive(Debug, PartialEq, Eq)]
@@ -55,17 +57,101 @@ fn parse_posting_assertion(line: &mut String) -> Result<Option<String>, ParseErr
         }))
 }
 
-fn parse_posting_amount(line: &mut String) -> Result<Option<PostingAmount>, ParseError> {
-    Ok(regex::Regex::new(POSTING_AMOUNT_REGEX)
+fn parse_posting_indent(line: &mut String) -> Result<(), ParseError> {
+    let Some(ws) = regex::Regex::new(r"^   *")
         .unwrap()
         .captures(&line.clone())
-        .map(|results| {
-            *line = line[results.get(0).unwrap().len()..].to_string();
-            PostingAmount {
-                commodity: results.get(1).map(|x| x.as_str().to_owned()),
-                amount: results.get(2).unwrap().as_str().to_owned(),
-            }
-        }))
+        .map(|results| results.get(0).unwrap().as_str().len())
+    else {
+        return Err(ParseError::Fail(
+            line.clone(),
+            "Expected at least two spaces.".to_string(),
+        ));
+    };
+
+    *line = line[ws..].to_string();
+    Ok(())
+}
+
+fn parse_account(line: &mut String) -> Result<String, ParseError> {
+    let Some(account) = regex::Regex::new(r"^[^ ]+")
+        .unwrap()
+        .captures(&line.clone())
+        .map(|results| results.get(0).unwrap().as_str().to_owned())
+    else {
+        return Err(ParseError::Fail(
+            line.clone(),
+            "Expected account name.".to_string(),
+        ));
+    };
+
+    *line = line[account.len()..].to_string();
+    Ok(account)
+}
+
+fn parse_commodity(line: &mut String) -> Result<Option<String>, ParseError> {
+    let commodity = regex::Regex::new(COMMODITY_REGEX)
+        .unwrap()
+        .captures(&line.to_owned())
+        .map(|results| results.get(0).unwrap().as_str().to_owned());
+    if let Some(x) = &commodity {
+        *line = line[x.len()..].to_string();
+    };
+
+    Ok(commodity)
+}
+
+fn parse_amount(line: &mut String) -> Result<Option<String>, ParseError> {
+    match regex::Regex::new(AMOUNT_REGEX)
+        .unwrap()
+        .captures(&line.to_owned())
+        .map(|results| results.get(0).unwrap().as_str().to_owned())
+    {
+        Some(amount) => {
+            *line = line[amount.len()..].to_string();
+            Ok(Some(amount))
+        }
+        None => Ok(None),
+    }
+}
+
+fn parse_whitespace(line: &mut String) -> Result<(), ParseError> {
+    match regex::Regex::new(WHITESPACE_REGEX)
+        .unwrap()
+        .captures(&line.clone())
+        .map(|results| results.get(0).unwrap().as_str().to_owned())
+    {
+        Some(ws) => {
+            *line = line[ws.len()..].to_string();
+            Ok(())
+        }
+        None => Err(ParseError::Fail(
+            line.clone(),
+            "Could not parse whitespace.".to_string(),
+        )),
+    }
+}
+
+fn parse_posting_amount(line: &mut String) -> Result<Option<PostingAmount>, ParseError> {
+    let mut nested_line = line.clone();
+    let commodity = parse_commodity(&mut nested_line)?;
+    parse_whitespace(&mut nested_line)?;
+    let amount = parse_amount(&mut nested_line)?;
+
+    match (&commodity, &amount) {
+        (None, None) => Ok(None),
+        (Some(_), None) => Err(ParseError::Fail(
+            line.clone(),
+            "Found commodity but no amount.".to_string(),
+        )),
+        (_, Some(amount_)) => {
+            *line = nested_line;
+            Ok(Some(PostingAmount {
+                commodity,
+                amount: amount_.clone(),
+            }))
+        }
+    }
 }
 
 fn parse_comment(line: &mut String) -> Result<Option<String>, ParseError> {
@@ -79,22 +165,18 @@ fn parse_comment(line: &mut String) -> Result<Option<String>, ParseError> {
 }
 
 fn parse_line_posting(line: &str) -> Result<LedgerLine, ParseError> {
-    let mut parsed_line = line.to_string();
-    let re = regex::Regex::new(POSTING_ACCOUNT_REGEX).unwrap();
-    let Some(results) = re.captures(&parsed_line) else {
-        return Err(ParseError::Fail(
-            parsed_line,
-            "Could not parse posting".to_owned(),
-        ));
-    };
+    let mut nested_line = line.to_string().clone();
 
-    let account = results.get(1).unwrap().as_str().to_owned();
-    parsed_line = parsed_line[results.get(0).unwrap().len()..].to_string();
-
-    let left_amount = parse_posting_amount(&mut parsed_line)?;
-    let assertion = parse_posting_assertion(&mut parsed_line)?;
-    let right_amount = parse_posting_amount(&mut parsed_line)?;
-    let comment = parse_comment(&mut parsed_line)?;
+    parse_posting_indent(&mut nested_line)?;
+    let account = parse_account(&mut nested_line)?;
+    parse_whitespace(&mut nested_line)?;
+    let left_amount = parse_posting_amount(&mut nested_line)?;
+    parse_whitespace(&mut nested_line)?;
+    let assertion = parse_posting_assertion(&mut nested_line)?;
+    parse_whitespace(&mut nested_line)?;
+    let right_amount = parse_posting_amount(&mut nested_line)?;
+    parse_whitespace(&mut nested_line)?;
+    let comment = parse_comment(&mut nested_line)?;
 
     Ok(LedgerLine::Posting(PostingLine {
         account,
@@ -176,6 +258,29 @@ mod test {
     }
 
     #[test]
+    fn test_it_parses_commodity() {
+        let jpy = Some("JPY".to_string());
+        assert_eq!(Ok(jpy.clone()), parse_commodity(&mut "JPY".to_string()));
+        assert_eq!(Ok(jpy.clone()), parse_commodity(&mut "JPY   ".to_string()));
+    }
+
+    #[test]
+    fn test_it_parses_amount() {
+        assert_eq!(
+            Ok(Some("1,000,000".to_string())),
+            parse_amount(&mut "1,000,000   ".to_string())
+        );
+        assert_eq!(
+            Ok(Some("-1,000,000".to_string())),
+            parse_amount(&mut "-1,000,000".to_string())
+        );
+        assert_eq!(
+            Ok(Some("-1000000.0".to_string())),
+            parse_amount(&mut "-1000000.0".to_string())
+        );
+    }
+
+    #[test]
     fn test_it_parses_posting() {
         for line in [
             "  asset:foobar  JPY 0",
@@ -191,6 +296,22 @@ mod test {
                     assertion: None,
                     right_amount: None,
                     comment: None
+                })),
+                parse_line_posting(line)
+            )
+        }
+
+        for line in ["  expense:foobar  JPY  123  ; 2025/04/01 12:34:56,foobar"] {
+            assert_eq!(
+                Ok(LedgerLine::Posting(PostingLine {
+                    account: "expense:foobar".to_owned(),
+                    left_amount: Some(PostingAmount {
+                        commodity: Some("JPY".to_owned()),
+                        amount: "123".to_owned()
+                    }),
+                    assertion: None,
+                    right_amount: None,
+                    comment: Some(" 2025/04/01 12:34:56,foobar".to_owned())
                 })),
                 parse_line_posting(line)
             )
